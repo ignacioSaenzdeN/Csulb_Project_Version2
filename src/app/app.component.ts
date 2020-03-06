d55ce863a9cce1ac3c42870a47cfc17b47af25be
nachomanchis <45250247+nachomanchis@users.noreply.github.com>
2020-03-05 19:30:55 -0800

    User class changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthenticationService } from './_services';
-
+import { User } from './_models';
 @Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent {
-    currentUser: any;
+    currentUser: User;
     // user subscr to db
     constructor(
         private router: Router,
