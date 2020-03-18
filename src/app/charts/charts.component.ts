6a9883e90bf1799b8442f3fc443dc30df86ccfcd
nachomanchis <45250247+nachomanchis@users.noreply.github.com>
2020-03-17 21:35:51 -0700

    test 1 almost finalized

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Chart} from 'chart.js';
 @Component({
   selector: 'app-charts',
   templateUrl: './charts.component.html',
   styleUrls: ['./charts.component.less']
 })
 export class ChartsComponent implements OnInit {
-
+  //addition
+  title: 'Graph';
+  chart = [];
   constructor() { }
-
   ngOnInit() {
-  }
+
+    this.chart = new Chart ('canvas', {
+      type:'line',
+
+      data:{
+        //labels are in one of the coordinates
+        //Eg: months (JAN,FEB,ETC.)
+        labels:[2,4,6,8,10,12],
+        //datasets are the elements to be charted, the lines
+        // each {} is one line/function
+        datasets:[
+          {
+          label: 'First dataset',
+          data: [100,200,300,200,400,250,600],
+          backgroundColor: 'red',
+          borderColor: 'red',
+          fill: false,
+        },
+        {
+          label: 'Second dataset',
+          data: [200,400,600,400,800,500,1200],
+          backgroundColor: 'blue',
+          borderColor: 'blue',
+          fill: false,
+        }//end of second dataset
+      ]//end of dataset
+      }//end of data
+    })// enf of this.chart
+  }// en of ngOnInit()
 
 }
